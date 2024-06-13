6ca327108079f32405cfaf351e23eaca7c347456
AZKY12 <79957498+AZKY12@users.noreply.github.com>
2024-06-13 20:37:48 +0530

    Create dbcon.php

---
+++ dbcon.php
@@ -0,0 +1,11 @@
+<?php
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "student";
+
+// Create connection
+$connection = mysqli_connect($servername, $username,$password, $dbname);
+
+?>
